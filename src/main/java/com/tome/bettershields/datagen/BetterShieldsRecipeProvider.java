dd1450761c9d375da07d23d98b5546cc64f425c7
ToMe25 <38815969+ToMe25@users.noreply.github.com>
2020-09-25 12:10:45 +0200

    Add jei as a optional runtime dependency
    rename recipe advancement has_item requirements

--- src/main/java/com/tome/bettershields/datagen/BetterShieldsRecipeProvider.java
+++ src/main/java/com/tome/bettershields/datagen/BetterShieldsRecipeProvider.java
@@ -28,15 +28,15 @@ public class BetterShieldsRecipeProvider extends RecipeProvider {
 		ShapedRecipeBuilder.shapedRecipe(BetterShields.ironShield).key(SHIELD_CHAR, Items.SHIELD)
 				.key(MATERIAL_CHAR, Tags.Items.INGOTS_IRON)
 				.patternLine("" + MATERIAL_CHAR + SHIELD_CHAR + MATERIAL_CHAR).patternLine(" " + MATERIAL_CHAR + " ")
-				.addCriterion("has_item", hasItem(Tags.Items.INGOTS_IRON)).build(consumer);
+				.addCriterion("has_iron_ingot", hasItem(Tags.Items.INGOTS_IRON)).build(consumer);
 		ShapedRecipeBuilder.shapedRecipe(BetterShields.goldShield).key(SHIELD_CHAR, Items.SHIELD)
 				.key(MATERIAL_CHAR, Tags.Items.INGOTS_GOLD)
 				.patternLine("" + MATERIAL_CHAR + SHIELD_CHAR + MATERIAL_CHAR).patternLine(" " + MATERIAL_CHAR + " ")
-				.addCriterion("has_item", hasItem(Tags.Items.INGOTS_GOLD)).build(consumer);
+				.addCriterion("has_gold_ingot", hasItem(Tags.Items.INGOTS_GOLD)).build(consumer);
 		ShapedRecipeBuilder.shapedRecipe(BetterShields.diamondShield).key(SHIELD_CHAR, Items.SHIELD)
 				.key(MATERIAL_CHAR, Tags.Items.GEMS_DIAMOND)
 				.patternLine("" + MATERIAL_CHAR + SHIELD_CHAR + MATERIAL_CHAR).patternLine(" " + MATERIAL_CHAR + " ")
-				.addCriterion("has_item", hasItem(Tags.Items.GEMS_DIAMOND)).build(consumer);
+				.addCriterion("has_diamond", hasItem(Tags.Items.GEMS_DIAMOND)).build(consumer);
 		specialRecipe(consumer, ShieldRecipes.SERIALIZER);
 	}
 
